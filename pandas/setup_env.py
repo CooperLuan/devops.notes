35b3fe432591ce7b7f4ef5974e32dcd4d459a538
Cooper.luan <gc.suprs@gmail.com>
2014-10-31 10:05:13 +0800

    add new note

--- pandas/setup_env.py
+++ pandas/setup_env.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from IPython.display import display_pretty
+from IPython.display import display_pretty, display_html
 import logging
 log = logging.getLogger('note')
 log.setLevel(logging.INFO)
